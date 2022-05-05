aa965dd027a3bd76950036e0e8753b3776319a0a
David <davidlboyd1986@gmail.com>
2022-05-05 10:58:46 -0400

    First submission. Will fix errors and clean up code later

--- src/Permutation.java
+++ src/Permutation.java
@@ -5,8 +5,9 @@ public class Permutation {
     public static void main(String[] args) {
         int num_of_permutations = Integer.parseInt(args[0]);
         RandomizedQueue<String> queue = new RandomizedQueue<String>();
-        while (StdIn.hasNextLine()) {
-            queue.enqueue(StdIn.readString());
+        String[] input_file = StdIn.readAllLines();
+        for (String value : input_file) {
+            queue.enqueue(value);
         }
         for (int i = 0; i < num_of_permutations; i++) {
             System.out.println(queue.dequeue());
